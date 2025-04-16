9ea8c4c4ff098831859f24b868076d1d3d91f6ba
teddy625 <teddy.loves2578@gmail.com>
2025-04-16 18:41:05 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botOwner: 'Knight Bot', // Your name
   ownerNumber: '917023951514', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
